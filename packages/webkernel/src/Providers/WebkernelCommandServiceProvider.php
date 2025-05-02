9fc076294abd08b5091d4278d60af99f7a7ea9b0
numerimondes <165125632+numerimondes@users.noreply.github.com>
2025-05-02 15:47:58 +0100

    updates

--- packages/webkernel/src/Providers/WebkernelCommandServiceProvider.php
+++ packages/webkernel/src/Providers/WebkernelCommandServiceProvider.php
@@ -18,29 +18,38 @@ class WebkernelCommandServiceProvider extends ServiceProvider
      * Register commands if in console mode.
      */
     protected function registerWebkernelCommands(): void
-{
-    if ($this->app->runningInConsole()) {
+    {
+        if ($this->app->runningInConsole()) {
 
-        $commandNamespace = 'Webkernel\\Commands\\';
-        $commandPath = base_path('packages/webkernel/src/Commands');
+            $commandNamespace = 'Webkernel\\Commands\\';
+            $commandPath = base_path('packages/webkernel/src/Commands');
 
-        $mainCommands = collect(glob("{$commandPath}/*.php"))
-            ->map(fn($file) => $commandNamespace . basename($file, '.php'))
-            ->filter(fn($class) => class_exists($class) && is_subclass_of($class, \Illuminate\Console\Command::class));
+            // Main commands in the Webkernel/Commands directory
+            $mainCommands = collect(glob("{$commandPath}/*.php"))
+                ->map(fn($file) => $commandNamespace . basename($file, '.php'))
+                ->filter(fn($class) => class_exists($class) && is_subclass_of($class, \Illuminate\Console\Command::class));
 
+            // Subcommands located in the composer/PostCreateProjectCmd directory
+            $subCommandPath = "{$commandPath}/composer/PostCreateProjectCmd";
+            $subCommandNamespace = $commandNamespace . 'composer\\PostCreateProjectCmd\\';
 
-        $subCommandPath = "{$commandPath}/composer/PostCreateProjectCmd";
-        $subCommandNamespace = $commandNamespace . 'composer\\PostCreateProjectCmd\\';
+            $subCommands = collect(glob("{$subCommandPath}/*.php"))
+                ->map(fn($file) => $subCommandNamespace . basename($file, '.php'))
+                ->filter(fn($class) => class_exists($class) && is_subclass_of($class, \Illuminate\Console\Command::class));
 
-        $subCommands = collect(glob("{$subCommandPath}/*.php"))
-            ->map(fn($file) => $subCommandNamespace . basename($file, '.php'))
-            ->filter(fn($class) => class_exists($class) && is_subclass_of($class, \Illuminate\Console\Command::class));
+            // Commands related to PackageManager
+            $packageManagerCommandPath = "{$commandPath}/PackageManager";
+            $packageManagerNamespace = $commandNamespace . 'PackageManager\\';
 
+            $packageManagerCommands = collect(glob("{$packageManagerCommandPath}/*.php"))
+                ->map(fn($file) => $packageManagerNamespace . basename($file, '.php'))
+                ->filter(fn($class) => class_exists($class) && is_subclass_of($class, \Illuminate\Console\Command::class));
 
-        $allCommands = $mainCommands->merge($subCommands)->toArray();
+            // Merge all commands
+            $allCommands = $mainCommands->merge($subCommands)->merge($packageManagerCommands)->toArray();
 
-
-        $this->commands($allCommands);
+            // Register all commands
+            $this->commands($allCommands);
+        }
     }
-}
 }
