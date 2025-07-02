1de041c31a79aa3662bc12bd631ee5c6fb44711b
numerimondes <165125632+emyassine@users.noreply.github.com>
2025-07-02 14:14:47 +0100

    Initial Commit

--- packages/webkernel/src/Constants/Application.php
+++ packages/webkernel/src/Constants/Application.php
@@ -5,8 +5,8 @@ use Illuminate\Foundation\Application as LaravelApplication;
 
 class Application extends LaravelApplication
 {
-    const WEBKERNEL_VERSION = '0.0.17';
-    const STABLE_VERSION = '0.0.17';
+    const WEBKERNEL_VERSION = '0.0.18';
+    const STABLE_VERSION = '0.0.18';
 
     const WEBKERNEL_PACKAGES = [
         'webkernel' => [
