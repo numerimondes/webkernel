affccad38cd7bee4f547610fa2b1597da470fc8e
numerimondes <165125632+emyassine@users.noreply.github.com>
2025-07-07 21:36:59 +0100

    Almost Good

--- platform/Modules/ReamMar/Core/Constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Numerimondes\Modules\ReamMar\Core;
-
-class Constants
-{
-    public const VERSION = '1.0.0';
-    public const ENABLED = true;
-}
--- platform/Modules/ReamMar/Mpr/Constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Numerimondes\Modules\ReamMar\Mpr;
-
-class Constants
-{
-    public const VERSION = '0.999.999'; // Maps to 1.0.0
-    public const ENABLED = true;
-}
