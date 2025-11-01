5799459e99635d67f38891dffc50e14a856a8d01
numerimondes <165125632+emyassine@users.noreply.github.com>
2025-11-01 23:41:36 +0100

    New Arch

--- platform/EnjoyTheWorld/Resources/Views/software/body/07-video-wrap.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Video Section -->
-<div class="video-wrap">
-   <a class="video-btn video-effect" data-fancybox="" href="#"
-      ><i class="isax isax-play5"></i
-      ></a>
-</div>
-<!-- /Video Section -->
